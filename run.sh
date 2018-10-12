df856ce028e911c68a1383e154b8f0af2bcadf37
Kaj Koski <koski.kaj@gmail.com>
2018-10-12 15:52:15 +0300

    fix

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ rm -f ./output/*.html
 rm -f ./output/*.xml
 rm -f ./output/*.png
 
-echo "start selenium to background"
-/opt/bin/entry_point.sh > /dev/null 2>&1 &
+#echo "start selenium to background"
+#/opt/bin/entry_point.sh > /dev/null 2>&1 &
 
-echo "wait for selenium"
-./wait-for-selenium.sh "localhost:4444" "./run_robot.sh"
+#echo "wait for selenium"
+#./wait-for-selenium.sh "localhost:4444" "./run_robot.sh"
 
 # Kill selenium
-ps uax | grep "selenium\|xvfb" | awk '{print $2}' | xargs kill -9 > /dev/null
+#ps uax | grep "selenium\|xvfb" | awk '{print $2}' | xargs kill -9 > /dev/null
 
 exit 0
